ef811603da1a3707e7cd4a27bc56071b13c98092
Anh Pham <phamanhqb73@gmail.com>
2023-12-11 13:38:36 +0700

    base 2

---
+++ WebApplication1/WebApplication1/Models/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApplication1.Models
+{
+    public partial class Order
+    {
+        public Order()
+        {
+            OrderDetails = new HashSet<OrderDetail>();
+        }
+
+        public int OrderId { get; set; }
+        public int? MemberId { get; set; }
+        public decimal? UnitPrice { get; set; }
+        public int? Quantity { get; set; }
+        public bool? Discount { get; set; }
+
+        public virtual Member? Member { get; set; }
+        public virtual ICollection<OrderDetail> OrderDetails { get; set; }
+    }
+}
